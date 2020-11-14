5ce55accff0a9d0f041bff8bd7128ffd8181c9a0
Jake Burton <40278490@live.napier.ac.uk>
2020-11-14 18:58:13 +0000

    Working Scraper - gets the first text field

--- scraper.py
+++ scraper.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+options = FirefoxOptions()
+fp = webdriver.FirefoxProfile()
+fp.set_preference("javascript.enabled", True)
+options.add_argument("--headless")
+# options.add_argument("--enable-javascript")
 
-#import time
-driver = webdriver.Firefox()
+
+driver = webdriver.Firefox(firefox_profile=fp, options=options)
 driver.get("""https://www.nvidia.com/en-gb/shop/geforce/?page=1&limit=9&locale=en-gb&search=1660""")
 
 page = driver.page_source
 soup = BeautifulSoup(page, 'html5lib')
-title = soup.find("div")
-print(title)
+title = soup.find("h2")
+print(title.get_text())
